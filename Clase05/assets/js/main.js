4cfe1021111d9dfd24c62145165fef57212c75e5
msubotich <mfsgib@gmail.com>
2022-09-22 23:55:11 -0300

    Clase 05

--- Clase05/assets/js/main.js
+++ Clase05/assets/js/main.js
@@ -74,3 +74,60 @@ console.log(ana);
 console.log(juan);
 ana.saludar();
 juan.saludar();
+
+//ver si exsite una propiedad en un objeto
+console.log('nombre' in ana);
+
+//recorrer todas las propiedades en un objeto
+
+for (const key in ana) {
+    console.log(ana[key]);
+}
+
+//**************CONCEPTO DE CLASES**************
+//No es nativo de js pero se incorporó posteriormente (ECMA6)
+
+class Rectangulo{
+    //propiedades
+    constructora(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    //metodos
+    calcularArea(){return this.x * this.y;}
+    calcularPerimetro(){return (this.x + this.y)*2;}
+}
+
+let rect1 = new Rectangulo(2,3);
+console.log(rect1.calcularArea());
+console.log(rect1.calcularPerimetro());
+
+/*Ejercicio
+ se necesita registrar información de clientes para calcular el impuesto anual según la 
+ fórmula:(( montoBrutoAnual - deducciones) * 0.21)
+ Usar ECMA6 con clases u métodos específicos.*/
+
+class Cliente{
+    constructor(cliente,impuestos){
+        this.cliente = cliente;
+        this.impuestos = impuestos;
+    }
+
+    calcularImpuesto(){
+        return `El impuesto total a pagar es: ${(this.impuestos.montoBrutoAnual - this.impuestos.deducciones) * 0.21} `;
+        
+    }
+}
+
+class Impuesto{
+    constructor(montoBrutoAnual,deducciones){
+        this.montoBrutoAnual = montoBrutoAnual;
+        this.deducciones = deducciones;
+    }
+}
+
+let impuesto1 = new Impuesto( 100000 , 2000 );
+let cliente1 = new Cliente('juan', impuesto1);
+
+console.log(cliente1.calcularImpuesto());
